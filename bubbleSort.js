430cc7fc63202ac2a93a9a19c5a8a9cea9240992
Ketan Deshmukh <deshmukh.ketan40@gmail.com>
2019-08-25 20:16:48 -0700

    alternate approach for bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,18 +1,37 @@
 const bubbleSort = (arr) => {
-    const swapNumber = (arr, a, b) => {
-        let temp = arr[a];
-        arr[a] = arr[b]
-        arr[b] = temp;
-    }
-
     for(let i = 0; i < arr.length; i++) {
         for(let j = i + 1 ; j < arr.length; j++) {
             if(arr[i] > arr[j]) {
-                swapNumber(arr, i , j);
+                swapHelperFunction(arr, i , j);
             }
         }
     }
     console.log(arr);
 }
 
-bubbleSort([1,5,3,6,8,9,0]);
+
+const swapHelperFunction = (arr, a, b) => {
+    let temp = arr[a];
+    arr[a] = arr[b]
+    arr[b] = temp;
+}
+
+let arr = [1,5,3,6,8,9,0];
+bubbleSort(arr);
+
+
+// alternate solution for better tiem complexity
+
+
+const bubbleSortBitBetter = (array) => {
+   let swapped;
+   do{
+       swapped = false;
+       for(let i = 0; i < array.length; i++) {
+        if(array[i] && array[i + 1] && array[i] > array[i + 1]) {
+            swapHelperFunction(array, i, i + 1);
+            swapped = true;
+        }
+      }
+   }while(swapped);
+}
