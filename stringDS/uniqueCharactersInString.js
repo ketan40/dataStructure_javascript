1a7f0692977526a124fdb8da6b543b65ecffe05c
kedeshmu <kedeshmu@cisco.com>
2019-09-02 21:01:35 -0700

    solution for cracking the coding interview problems :- string and arrays

--- stringDS/uniqueCharactersInString.js
+++ stringDS/uniqueCharactersInString.js
@@ -13,4 +13,23 @@ const checkIfHasUniqueCharactersInString = (charString) => {
        console.log(true);
        return true;
 }
-checkIfHasUniqueCharactersInString("bhsgyu");
+checkIfHasUniqueCharactersInString("bhsgyuy");
+
+
+
+//another implmentation if asked not to use another implmentation (you are okay to modify the inoust string)
+
+// const checkIfHasUniqueCharactersInStringAnotherImpl = (charString) => {
+//     let i = 0;
+
+//     for(let j = 0; j < charString.length ; j++) {
+//         if(charString.substr(i + 1,charString.length).includes(charString.substr(i, 1))) {
+//             console.log(false);
+//             return false;
+//         }
+//         charString = charString.substr(i + 1, charString.length);
+//     }
+//     console.log(true);
+//     return true
+// }
+// checkIfHasUniqueCharactersInStringAnotherImpl("bhsgyub");
